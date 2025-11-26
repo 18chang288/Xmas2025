1b7cad7278d4c2791572898d0659b87c37ec77f6
lchang288 <changlao2022@gmail.com>
2025-11-26 14:18:01 -0600

    deploy test 1.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { supabase } from './supabaseClient'
+import LandingPage from "./components/LandingPage";
 
 export default function App() {
   console.log(supabase)
-  return <h1>Supabase loaded!</h1>
+  return <LandingPage />
 }

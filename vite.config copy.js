e138059f4116c968e3ead655520173174aec537e
lchang288 <changlao2022@gmail.com>
2025-11-25 21:31:07 -0600

    deploy test

--- vite.config copy.js
+++ vite.config copy.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // ensures assets work on GitHub Pages
+  base: './', // use relative paths for GitHub Pages
 })
